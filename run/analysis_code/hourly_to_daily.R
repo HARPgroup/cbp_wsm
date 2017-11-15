f59b8301d81a9a29e8cd89bc0a4c58e1b18921d5
Alyssa Ford <32133601+alyssaf4@users.noreply.github.com>
2017-11-14 23:40:40 -0500

    Update hourly_to_daily.R

--- run/analysis_code/hourly_to_daily.R
+++ run/analysis_code/hourly_to_daily.R
@@ -1,11 +1,16 @@
 #This code translates the model data from hourly to data for comparison to the USGs data
-
-uri_model_output <- 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSbD4ZuD68FK9ZqESd-_kWlsMhd1a66LWr1N8Xd5oo5zFZm3niigHhFiUJ98b2Y9ayQdncu-4q5VvL4/pub?output=csv'
+#Loads hourly model data 
+#For riverseg OD5_8780_8660
+##uri_model_output <- 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSbD4ZuD68FK9ZqESd-_kWlsMhd1a66LWr1N8Xd5oo5zFZm3niigHhFiUJ98b2Y9ayQdncu-4q5VvL4/pub?output=csv'
+#For riverseg OD3_8630_8720
+uri_model_output <- 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQXaPe1n1ZCo-zsLCkrQTONz6-4QnNakzeIwJw7K5yr3vuEBeues9rFCfweAnw5R_cKIZCaLvFS2_0x/pub?output=csv'
 model_output = read.csv(uri_model_output, header = TRUE, sep = ",", stringsAsFactors = FALSE);
 
+# Creates empty data frame for daily data
 daily_data <- data.frame(matrix(nrow = 1, ncol = 4))
 colnames(daily_data) <- c("year", "month", "day", "ovol")
 
+# The 0.504167 conversion factor is used to convert acre-ft/day to ft^3/s
 z <- 1
 x <- model_output[1,]
 daily_data[z,1] <- x$year
