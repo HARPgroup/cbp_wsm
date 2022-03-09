d23c6f9b21af4047ab589ae4a4a1e63854322a03
Robert W. Burgholzer <rburghol@vt.edu>
2022-03-09 14:10:53 +0000

    Added NLDAS specific codeand generic wdm handlers

---
+++ run/export/wdm_export_flow.R
@@ -0,0 +1,27 @@
+# R script to export a wdm, combine into files
+library('lubridate')
+library('data.table')
+source('https://raw.githubusercontent.com/HARPgroup/cbp6/master/R/utils.R')
+
+# Read Args
+argst <- commandArgs(trailingOnly=T)
+scenario <- as.character(argst[1])
+lseg <- as.character(argst[2])
+startyear <- as.integer(argst[3])
+endyear <- as.integer(argst[4])
+outbase <- as.character(argst[5])
+wdmpath <- as.character(argst[6])
+
+outpath = paste(outbase, 'out/land', scenario, 'eos', sep='/')
+outname <- paste0(lseg, '_0111-0211-0411.csv' )
+
+wdm_export_land_flow(lseg, wdmpath, scenario, startyear, endyear)
+# use fast fread() mode, more than 2x speed enhancement
+merged_df1 <- wdm_merge_land_flow(lseg, wdmpath, scenario , outpath, TRUE, FALSE)
+# use slow mode
+# merged_df2 <- wdm_merge_land_flow(lseg, wdmpath, scenario , outpath)
+
+saved.file <- paste0(outpath, "/", outname)
+# also much faster than other version
+fwrite(merged_df1, saved.file, row.names = FALSE)
+message(paste("Exported file", saved.file))
