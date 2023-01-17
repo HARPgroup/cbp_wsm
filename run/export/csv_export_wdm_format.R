e2fad477d5a3607531129fc384ebcccd3ff2dc3e
Robert W. Burgholzer <rburghol@vt.edu>
2023-01-17 18:53:24 +0000

    catch up on p5 hsp2 scripts

--- run/export/csv_export_wdm_format.R
+++ run/export/csv_export_wdm_format.R
@@ -15,28 +15,16 @@ omsite = "http://deq1.bse.vt.edu:81"
 #hydr <- fread("OR1_7700_7980_hydr.csv") #for testing
 
 argst <- commandArgs(trailingOnly = T)
-hydr_path <- argst[1]
-#hydr_file <- ('OR1_7700_7980_hydr.csv')
-rovol_path <- argst[2]
-#rovol_file <- ('OR1_7700_7980_rovol.csv')
+input_path <- argst[1]
+output_path <- argst[2]
 column <- argst[3]
-#column <- 'ROVOL'
 
-
-hydr <- fread(hydr_path)
-
+hydr <- fread(input_path)
 hydr %>% select(column) -> hydr_column
-
-# the hydr.csv contains the year and month columns already
-hydr$hour <- hour(hydr$index)
-hydr$day <- day(hydr$index)
-hydr$month <- month(hydr$index)
-hydr$year <- year(hydr$index)
-
 
 # creating tables with OVOL3 and ROVOL
 hydr_df <- data.frame(hydr$year, hydr$month, hydr$day, hydr$hour, hydr_column)
 
 # exporting the tables
-write.table(hydr_df, file = rovol_path, sep = ',', row.names = FALSE, col.names = FALSE, quote = FALSE)
+write.table(hydr_df, file = output_path, sep = ',', row.names = FALSE, col.names = FALSE, quote = FALSE)
 
