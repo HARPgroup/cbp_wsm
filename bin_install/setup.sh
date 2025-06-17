85282b3bc20e5e7e8a7b8e1245a50d90cf2ccf04
Rob <rburghol@vt.edu>
2025-06-17 20:26:13 +0000

    up to date install instructions

--- bin_install/setup.sh
+++ bin_install/setup.sh
@@ -9,9 +9,12 @@ fi
 cp $PWD/bin_install/find_config $ip/find_config
 cp $PWD/bin_install/hspf_config $ip/hspf_config
 cp $PWD/bin_install/cbp $ip/cbp
-cp $PWD/bin_install/slurmq $ip/slurmq
 # get cbp config block
 cp $PWD/bin_install/get_config $ip/get_config
 cp $PWD/bin_install/hspf.config.default $PWD/hspf.config
-# slurm  meta model helpers
-cp $PWD/bin_install/mm_job_id $PWD/mm_job_id
+# mm scripts
+cp $PWD/run/mm/mm_job_id $ip/
+cp $PWD/run/mm/mm_job_name $ip/
+cp $PWD/run/mm/mm_river_deps $ip/
+cp $PWD/run/mm/mm_run_basin $ip/
+cp $PWD/run/mm/mm_run_land $ip/
